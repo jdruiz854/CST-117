cbaf6489c281de73ebd285cab8a6948195e0105f
jdruiz854 <hurrahfighter@gmail.com>
2021-03-21 19:08:02 -0400

    Add project files.

---
+++ ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace ProgrammingProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openButton.Location = new System.Drawing.Point(38, 30);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(102, 35);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(672, 30);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(102, 35);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(352, 30);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(102, 35);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.FormattingEnabled = true;
+            this.outputBox.Location = new System.Drawing.Point(38, 97);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(736, 342);
+            this.outputBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "File Processing";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.ListBox outputBox;
+    }
+}
+
---
+++ ProgrammingProject3/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingProject3
+{
+    public partial class Form1 : Form
+    {
+
+        StreamReader inputFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            processDataLowerCase();
+            processDataFirstAndLastWord();
+            processDataFindLongestWord();
+            processDataFindMostVowels();
+            printToText();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Items.Clear();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void processDataLowerCase()
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    string lowerCase;
+
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+
+                    while (inputFile.EndOfStream == false)
+                    {
+                        lowerCase = inputFile.ReadLine();
+
+                        lowerCase = lowerCase.ToLower();
+                        outputBox.Items.Add(lowerCase);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Operation canceled. ");
+            }
+        }
+
+        private void processDataFirstAndLastWord()
+        {
+            try
+            {
+                string firstWord;
+                string lastWord;
+
+                List<string> wordList = new List<string>();  
+
+                inputFile = File.OpenText(openFileDialog1.FileName);
+
+                firstWord = inputFile.ReadLine();
+                firstWord = firstWord.Substring(0, firstWord.IndexOf(" "));
+
+                lastWord = inputFile.ReadLine();
+                lastWord = lastWord.Split(' ').Last();
+
+                wordList.Add(firstWord);
+                wordList.Add(lastWord);
+
+                wordList.Sort();
+
+                for(int i = 0; i < wordList.Count; i++)
+                {
+                    outputBox.Items.Add(wordList[i]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Operation Canceled. ");
+            }
+        }
+
+        private void processDataFindLongestWord()
+        {
+            string longestWord;
+            string[] words;
+            int counter = 0;
+
+            inputFile = File.OpenText(openFileDialog1.FileName);
+
+            longestWord = inputFile.ReadToEnd();
+
+            words = longestWord.Split(new[] { " " }, StringSplitOptions.None);
+
+            foreach(String s in words)
+            {
+                if(s.Length > counter)
+                {
+                    longestWord = s;
+                    counter = s.Length;
+                    
+                }
+            }
+            outputBox.Items.Add(longestWord);
+        }
+
+        private void processDataFindMostVowels()
+        {
+            string mostVowels;
+            string[] words;
+            int vowel_count = 0;
+            int cons_count = 0;
+            int counter = 0;
+
+            inputFile = File.OpenText(openFileDialog1.FileName);
+
+            mostVowels = inputFile.ReadToEnd();
+
+            words = mostVowels.Split(new[] { " " }, StringSplitOptions.None);
+
+            foreach(String s in words)
+            {
+                if (vowel_count >= counter)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                        {
+                            vowel_count++;
+                        }
+                        else
+                        {
+                            cons_count++;
+                        }
+                    }
+
+                    mostVowels = s;
+                    counter = vowel_count;
+                }
+            }
+
+            outputBox.Items.Add(mostVowels);
+        }
+        private void printToText()
+        {
+            StreamWriter outputFile;
+            outputFile = File.CreateText("Output.txt");
+
+            foreach(String s in outputBox.Items)
+            {
+                outputFile.WriteLine(s);
+            }
+
+            outputFile.Close();
+        }
+    }
+}
