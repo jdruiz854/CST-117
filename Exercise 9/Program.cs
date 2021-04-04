a1296c6327650e5ff0d5141b06e4f117eba48771
jdruiz854 <hurrahfighter@gmail.com>
2021-04-04 17:31:31 -0400

    Add project files.

---
+++ Exercise 9/Program.cs
@@ -0,0 +1,45 @@
+﻿//correted By Joshua Ruiz
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            //changed to union before the A 
+            A.union(B);
+
+            Console.WriteLine("After Union operation");
+            //Console.WriteLine("A union B: " + A)
+            Console.WriteLine("A union B: " + A);
+            
+
+            //Removed this portion of code as it was redundant and did not display correctly.
+            //Console.WriteLine("After union operation");
+            //Console.WriteLine("A: " + A);
+            //Console.WriteLine("B: " + B);
+
+        }
+    }
+}
---
+++ Exercise 9/Set.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise_9
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+           //logical test changed to validate the existince of the new element in the list before adding to avoid duplicates.
+            if (elements.Contains(val)) 
+                return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        //private bool containsElement(int val)
+        //{
+           // for (int i = 0; i < elements.Count; i++)
+            //{
+              //  if (val == elements[i])
+                //    return true;
+                //else
+                  //  return false;
+            //}
+
+            //return false;
+        //}
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                this.addElement(rhs.elements[i]);
+            }
+            return rhs;
+        }
+    }
+
+}
+
